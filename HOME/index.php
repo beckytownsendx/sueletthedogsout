4b9240d580c562e9dc5fa1f0073830846d6fffeb
Becky <becky-townsend@hotmail.com>
2017-07-19 17:51:15 +0100

    files added

---
+++ HOME/index.php
@@ -0,0 +1,299 @@
+<body>
+  <!--Nav Bar-->
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="nav">
+      <div class="container-fluid">
+
+        <div class="col-md-6">
+
+
+
+          <img src="http://i65.tinypic.com/2rn7kfa.png" height="70" />
+        </div>
+
+        <div class="col-md-6">
+
+          <a href="#top"><span class="glyphicon glyphicon-home" aria-hidden="true" style="color:white"></span></a>
+          <a href="#about_me"><span class="glyphicon glyphicon-user" aria-hidden="true" style="color:white"></span></a>
+          <a href="#contact"><span class="glyphicon glyphicon-earphone" aria-hidden="true" style="color:white"></span></a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!--Nav Bar-->
+  <a name="top"></a>
+  <!--Jumbotron-->
+  <div class="jumbotron">
+    <video id="video-background" preload muted autoplay loop>
+      <source src="https://pixabay.com/en/videos/download/video-3518_small.mp4" type="video/mp4">
+    </video>
+    <div class="container text-center">
+      <div class="logo">
+        <img src="http://i65.tinypic.com/2rn7kfa.png" height="350" />
+      </div>
+    </div>
+  </div>
+  <!--Jumbotron-->
+
+
+
+  <!-- About Me-->
+  <a name="about_me"></a>
+
+  <div class="parrallax2">
+    <div class="container-fluid">
+      <div class="about-me">
+        <h1>
+Dogs are like chocolates, you can't have just one!
+</h1>
+        <p>
+          Hi! My name is Sue and I have a BIG heart for animals! I am a extremely caring, reliable and professional dog walker and house sitter living in Bristol I am a pet lover with the philosophy that all creatures deserve the best care and attention possible
+          and that being with them is as rewarding for us as it is for them. Your pet will be in the safest of hands with me. I offer dog walking - individually or in groups - in beautiful, traffic-free environments around Bristol. Giant breeds are welcome!
+          I am also able to provide a Pet Sitting service - whatever type of pet you have! Llamas to iguanas, just give me a call - I used to be a former Zookeeper!. I am fully insured and follow Bristol City Council's code for dog owners.
+
+          <br>
+          <br>
+        </p>
+
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Information-->
+  <div class="section1">
+    <div class="container-fluid">
+
+
+      <div class="row">
+        <div class="col-md-6 text-center bg-color2">
+
+
+          <h1>
+      House Sitting
+      </h1>
+
+          <p>
+            We offer overnight pet sitting in the client's home. This unique service is ideal for pet owners who, for example, want a few days or weeks' holiday but are reluctant to kennel board. The majority of cats and dogs, particularly dogs, suffer anxiety, stress
+            and depression due to unfamilar surroundings, lack of exercise, stimulation and human companionship. We provide a highly professional and personalised service to accomodate you and your pet's specific needs. We want you to have peace of mind
+            knowing that your pet will be relaxed at home in safe hands, following its usual routine, receiving walks/pet visits throughout the day and having loving care and companionship throughout the night.
+
+          </p>
+        </div>
+
+        <div class="col-md-6 text-center">
+          <img src="http://i68.tinypic.com/2ms0ig9.png" height="300px" />
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6 text-center">
+          <img src="http://i67.tinypic.com/10dbmuo.png" height="300px" />
+        </div>
+        <div class="col-md-6 text-center bg-color">
+          <h1>
+      Dog Walking
+      </h1>
+          <p>
+            We offer one hour dog walks individually or in a group at extremely competitive rates. The walks take place in safe, traffic free, varied environments such as riverside walks, woodlands and parks. We are happy to deal with all dogs, from the smallest
+            through to giant breeds. We're also happy to help out with "special needs" pets, just give us a call to discuss. We have a staff member with over 30 years experience in handling and working with dogs with behavioural problems. We will arrange
+            to pick up your dog in our purposely chosen van which provides all round vision, lots of fresh air and plenty of room to move. In addition we also carry travelling cages, fresh water, balls, toys and clean towels!
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6 text-center bg-color2">
+          <h1>
+      Areas Covered</h1>
+          <p>
+            We focus on serving the Eastville, Kingswood, St George's, Hanham, St Anne's, Easton, Stapleton and Redfield areas of Bristol. For Pet Sitting services, however, we may well be able to work further afield.
+            <br>
+            <br>
+            <br> The walks themselves take place in Eastville Park, Snuff Mills, Crews Hole, Dundridge, Oldbury Park and other exciting places as we find them!
+
+
+          </p>
+
+        </div>
+        <div class="col-md-6 text-center">
+          <img src="http://i65.tinypic.com/2rhlhlg.png" height="250" />
+        </div>
+      </div>
+      <div class="row">
+
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="parrallax">
+    <div class="container-fluid">
+      <div class="testamonial">
+        <h1>
+See what my client's have to say..
+</h1>
+        <p>
+          "Sue has been walking Effie our Scottish Terrier for over a year now and we're sure Effie sees her as her third owner! Our lovely Scottie gets so excited when she sees Sue coming towards the door and it's plain to see how much she loves both walking with
+          Sue and mixing with other dogs. "Sue's patience and love of dogs is evident when you first meet her and she imparts this love towards the dogs she walks. Scotties can be known as a stubborn, often difficult to train breed, but Sue has been able
+          to turn Effie into a well behaved little dog with a great personality!"
+          <br>
+          <br>
+          <b>- Lesley</b>
+          <br>
+          <br>
+        </p>
+        <div class="row">
+
+          <div class="col-md-12 text-center">
+
+   <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">READ MORE TESTIMONTIALS HERE!</button>
+          </div>
+          <br>
+          <br>
+          <br>
+        </div> <br>
+          <br>
+        
+      </div>
+    </div>
+  </div>
+  
+   <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">What my customers have to say about me..</h4>
+        </div>
+        <div class="modal-body">
+          <p>
+            "Our three dogs love “Auntie Sue” to bits! She is like a doggy ‘Pied Piper’ and is always loving, attentive and reliable. We can honestly say we would be lost without her."
+<br><br>
+Cory & Sarah
+<br><br>
+"Sue loves my dogs as I do and has always gone the extra mile when looking after them. I trust her absolutely and as a single woman, my dogs are everything to me but Sue is their ‘Second Mum’ - I would not feel happy allowing my ‘babies’ into the care of anyone else.I just hope she never leaves Bristol…I would be lost!"
+<br><br>
+Mel<br><br>
+
+"Sue has been walking Effie our Scottish Terrier for over a year now and we're sure Effie sees her as her third owner! Our lovely Scottie gets so excited when she sees Sue coming towards the door and it's plain to see how much she loves both walking with Sue and mixing with other dogs.Sue's patience and love of dogs is evident when you first meet her and she imparts this love towards the dogs she walks. Scotties can be known as a stubborn, often difficult to train breed, but Sue has been able to turn Effie into a well behaved little dog with a great personality!"
+<br><br>
+Lesley
+<br><br>
+"I recommend Sue to anyone looking for a reliable Pet Care and Dog Walking Service."
+<br><br>
+Jane
+<br><br>
+
+"Thank you so much Sue for all that you have done for me and Mickey, we are both happier than ever. Mickey loves his mum but his Auntie Sue has a very special place in his life."
+<br><br>
+Marie
+<br><br>
+"Sue has been walking my little puppy Bindi for three months and has been very reliable. Sue also has a co-worker and both have brought with them a professional service to me and my dog. This has enabled me to have a very happy little dog and go to work not worrying about leaving him. This has made my life a lot easier.I would recommend 'Sue... Let The Dogs Out' as she has proved to be trustworthy in our home whilst always showing Bindi lots of love and affection. Sue has also provided Bindi with many opportunities to socialise with other dogs which has been perfect for my young dog."
+<br><br>
+Dot
+<br><br>
+
+"Sue has been walking my dog for the last few weeks, he comes home happy and tired and can't wait for the next time. I find Sue reliable and extremely trustworthy with my dog and I am confident that he is in good hands when he is with her.Sue also trained him very well and made me more comfortable at letting him off the lead. I can heartily recommend her to anyone."
+<br><br>
+Kris
+<br><br>
+"I have a pet dog walker called Sue. She comes to take me out twice a week. On the days I am going out with her I know, coz my mummy tells me in the mornings "it's a Sue day." I get very excited and jump around and usually get my mum with a big wet lick.I love my time out with my pet Sue. She takes me to parks, woods and down by the river. I have such a good time. I go out with other dogs too. I have made many new friends since I have been walking with Sue.Once when I was out I wasn't feeling too good and got a bit dizzy and faint when I got in the car. Sue took me straight to the "dogtors" - I think you humans call them vets. She called my mummy from the vets and let her know.Sue takes good care of me when we are out which makes me and my mummy very happy. I have to say I would recommend my pet Sue to any canine friend out there. You will have a great time with her. She will take good care of you as if you were her own, and, who knows, we may meet up and become friends too.
+<br>
+Thank you Sue for all my lovely walks, pats and treats. I hope lots of other dogs will meet you like me and have just as much fun as I do."
+<br><br>
+Tadger<br><br>           
+            
+            
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+  
+  <!--Testamonial Module-->
+
+  <!-- Information-->
+  <!--Contact Form -->
+  <a name="contact"></a>
+  <div class="contact-form">
+    <div class="container text-center">
+      <div class="row">
+        <h1>
+  Contact me below..
+  </h1>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="form-group">
+            <label for="usr">Name:</label>
+            <input type="text" class="form-control" id="usr">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <label for="usr">Email:</label>
+          <input type="text" class="form-control" id="usr">
+        </div>
+        <div class="col-md-6">
+          <label for="usr">Phone Number:</label>
+          <input type="text" class="form-control" id="usr">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="form-group">
+            <label for="comment">Message:</label>
+            <textarea class="form-control" rows="5" id="comment"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <button type="button" class="btn btn-primary btn-lg">SUBMIT</button>
+        <br>
+        <br>
+        <br>
+      </div>
+
+
+      <h3>
+   Email: info@sueletthedogsout.co.uk<br><br>
+   Telephone: 07981287391<br><br>
+   </h3></div>
+  </div>
+
+  <!--Contact Form -->
+  <!-- Footer -->
+  <div class="footer">
+    <p>
+      Layout created by beckythedeveloper.co.uk 2017
+    </p>
+  </div>
+  <!--footer-->
+</body>
